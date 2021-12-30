16b9a5f77f3eb8b0b080c38617b53a9b2c505e9b
tarokent <t.8.p.1.dogg@gmail.com>
2021-12-30 15:20:52 +0900

    fix

--- src/views/flex_study/Index.vue
+++ src/views/flex_study/Index.vue
@@ -3,16 +3,16 @@
     <!-- flexbox 水平または垂直に要素を配置し柔軟なレイアウトを実現する -->
     <!-- flex container / flex item -->
     <div class="flex-container">FlexContainer</div>
+    <div class=""></div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-
 export default defineComponent({
   setup() {},
 });
 </script>
-<style lang="sass">
+<style>
 .flex-container {
   display: flexbox;
 }
